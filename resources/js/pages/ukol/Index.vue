092970cb0b37ae2db22d5c1b00709873f9ecb60f
Pavlina <pavlina.malinkova@email.cz>
2025-12-18 07:57:08 +0100

    some minor changes

--- resources/js/pages/ukol/Index.vue
+++ resources/js/pages/ukol/Index.vue
@@ -16,6 +16,7 @@ import {
 } from '@/components/ui/dropdown-menu';
 import { Link } from '@inertiajs/vue3';
 import { useDateFormat } from "@vueuse/core";
+import { Badge } from '@/components/ui/badge'
 
 defineProps({ ukoly: Object });
 </script>
@@ -23,7 +24,7 @@ defineProps({ ukoly: Object });
 <template>
   <h1 class="text-3xl mb-5">Seznam úkolů</h1>
   <div class="flex items-center gap-3">
-    <Link :href="create()" class="rounded bg-blue-700 text-white px-2 py-1"><button>Přidat úkol</button></Link>
+    <Link :href="create()" class="rounded bg-blue-700 text-white px-2 py-1">Přidat úkol</Link>
     <DropdownMenu>
       <DropdownMenuTrigger>Filtrování dle statusu</DropdownMenuTrigger>
       <DropdownMenuContent>
@@ -43,7 +44,22 @@ defineProps({ ukoly: Object });
   <Accordion type="single" collapsible class="w-full" default-value="item-1">
     <AccordionItem :value="index" v-for="(ukol, index) in ukoly" :key="ukol.id">
 
-      <AccordionTrigger class="py-5">{{ ukol.nazev }}</AccordionTrigger>
+      <AccordionTrigger class="py-6 cursor-pointer">
+
+        <div class="flex items-center gap-2">
+          <h2 class="text-2xl">{{ ukol.nazev }}</h2>
+          <Badge v-show="ukol.dokonceno === 1" variant="secondary" class="bg-blue-700 text-white dark:bg-blue-600">
+            <BadgeCheckIcon />
+            Hotovo
+          </Badge>
+
+          <Badge v-show="ukol.dokonceno !== 1" variant="destructive">
+            <BadgeCheckIcon />
+            Udělat!
+          </Badge>
+        </div>
+
+      </AccordionTrigger>
       <AccordionContent>
         <p>
           {{ ukol.popis }}
@@ -56,63 +72,13 @@ defineProps({ ukoly: Object });
           hotovo
         </div>
         <!-- <div>Hotovo do: {{ ukol.dokdy }}</div> -->
-        <Link :href="edit(ukol.id)" class="rounded bg-blue-700 text-white px-2 py-1"><button>Upravit úkol</button>
-        </Link>
-        <Link :href="destroy(ukol.id)" class="rounded bg-red-800 text-white px-2 py-1"><button>Smazat úkol</button>
-        </Link>
+        <Link :href="edit(ukol.id)" class="rounded bg-blue-700 text-white px-2 py-1">Upravit úkol</Link>
+        <Link :href="destroy(ukol.id)" class="rounded bg-red-800 text-white px-2 py-1 cursor-pointer">Smazat úkol</Link>
         </p>
       </AccordionContent>
     </AccordionItem>
 
 
   </Accordion>
-
-
-  <!-- <Accordion type="single" collapsible class="w-full" default-value="item-1">
-    <AccordionItem value="item-1">
-      <AccordionTrigger>Product Information</AccordionTrigger>
-      <AccordionContent>
-        <p>
-          Our flagship product combines cutting-edge technology with sleek
-          design. Built with premium materials, it offers unparalleled
-          performance and reliability.
-        </p>
-        <p class="mt-2">
-          Key features include advanced processing capabilities, and an
-          intuitive user interface designed for both beginners and experts.
-        </p>
-      </AccordionContent>
-    </AccordionItem>
-    <AccordionItem value="item-2">
-      <AccordionTrigger>Shipping Details</AccordionTrigger>
-      <AccordionContent>
-        <p>
-          We offer worldwide shipping through trusted courier partners.
-          Standard delivery takes 3-5 business days, while express shipping
-          ensures delivery within 1-2 business days.
-        </p>
-        <p class="mt-2">
-          All orders are carefully packaged and fully insured. Track your
-          shipment in real-time through our dedicated tracking portal.
-        </p>
-      </AccordionContent>
-    </AccordionItem>
-    <AccordionItem value="item-3">
-      <AccordionTrigger>Return Policy</AccordionTrigger>
-      <AccordionContent>
-        <p>
-          We stand behind our products with a comprehensive 30-day return
-          policy. If you're not completely satisfied, simply return the
-          item in its original condition.
-        </p>
-        <p class="mt-2">
-          Our hassle-free return process includes free return shipping and
-          full refunds processed within 48 hours of receiving the returned
-          item.
-        </p>
-      </AccordionContent>
-    </AccordionItem>
-  </Accordion> -->
-
 
 </template>
